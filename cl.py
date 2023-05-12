44a5c5fc9399733f5cc82f862b4c3568186782a5
Charitha <99468821+charitha-darapaneni@users.noreply.github.com>
2023-05-12 07:42:00 -0500

    cl.py

--- cl.py
+++ cl.py
@@ -19,37 +19,36 @@ df_c.to_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocesse
 
 
 # converting datatype of votes
-# df = pd.read_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
-#
-#
-# def conv(value):
-#     if isinstance(value, str):
-#         return int(value.replace(',', ''))
-#     elif isinstance(value, float) and not math.isnan(value):
-#         return int(value)
-#     elif pd.isna(value):
-#         return value
-#
-#
-# df['votes'] = df['votes'].apply(conv)
-#
-# df.to_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
+df = pd.read_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
+
+
+def conv(value):
+    if isinstance(value, str):
+        return int(value.replace(',', ''))
+    elif isinstance(value, float) and not math.isnan(value):
+        return int(value)
+    elif pd.isna(value):
+        return value
+
+
+df['votes'] = df['votes'].apply(conv)
+
+df.to_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
 
 
 # converting rating datatype into float
 df = pd.read_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
-# df['rating'] = df['rating'].astype(float)
-# df.to_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
+df['rating'] = df['rating'].astype(float)
 
 # converting duration
-# df['duration'] = df['duration'].str.replace('min', '').fillna(0)
-# df['duration'] = df['duration'].astype(int)
-#
-# df['startyear'] = df['startyear'].fillna(0).astype(int)
-# df['endyear'] = df['endyear'].replace(' ', pd.NaT)
-# df['endyear'] = df['endyear'].fillna(0).astype(int)
-#
-# df['votes'] = df['votes'].fillna(0).astype(int)
+df['duration'] = df['duration'].str.replace('min', '').fillna(0)
+df['duration'] = df['duration'].astype(int)
+
+df['startyear'] = df['startyear'].fillna(0).astype(int)
+df['endyear'] = df['endyear'].replace(' ', pd.NaT)
+df['endyear'] = df['endyear'].fillna(0).astype(int)
+
+df['votes'] = df['votes'].fillna(0).astype(int)
 df['certificate'] = df['certificate'].fillna(0).astype(str)
 df['title'] = df['title'].fillna(0).astype(str)
 df.to_csv(r'C:\umkc\courses\cc\proj\netflix popular movies dataset\preprocessed_data.csv')
